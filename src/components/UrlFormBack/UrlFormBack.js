8e3a56c571069f5d08b4c2a67378b862e0e48510
ruucm <ruucm.a@gmail.com>
2018-05-03 03:42:32 +0900

    fmt: UrlFormBack
    remove unusing code

--- src/components/UrlFormBack/UrlFormBack.js
+++ src/components/UrlFormBack/UrlFormBack.js
@@ -21,17 +21,6 @@ class UrlFormBack extends Component {
     const { w, h, children, idx, yellowSize, clr } = this.props;
     return (
       <g id="Canvas" fill="none">
-        <filter id="dropshadow" height="130%">
-          <feGaussianBlur in="SourceAlpha" stdDeviation="3"/>
-          <feOffset dx="2" dy="2" result="offsetblur"/>
-          <feComponentTransfer>
-            <feFuncA type="linear" slope="0.5"/>
-          </feComponentTransfer>
-          <feMerge> 
-            <feMergeNode/>
-            <feMergeNode in="SourceGraphic"/>
-          </feMerge>
-        </filter>
         <g id="content">
         <path d={this.roundedRect(0, 0, w, h, 21)} fill="white"/>
         </g>
