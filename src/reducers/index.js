1ea8fde5d93520cc10db7c323a2db84a071357d2
ruucm <ruucm.a@gmail.com>
2018-05-03 09:12:47 +0900

    fix: UrlForm
    combine reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import canvas from './canvas';
 import submitTest from './submitTest';
+import { reducer as reduxFormReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   canvas,
   submitTest,
+  form: reduxFormReducer,
 });
 
 export default rootReducer;
