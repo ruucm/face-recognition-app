28af1be67d3f15f16ce856220360a54b61a286f2
ruucm <ruucm.a@gmail.com>
2018-05-04 17:06:58 +0900

    fix: UrlFormBack
    only radius at top

--- src/components/UrlFormBack/UrlFormBack.js
+++ src/components/UrlFormBack/UrlFormBack.js
@@ -4,8 +4,6 @@ import { log } from 'ruucm-util';
 
 class UrlFormBack extends Component {
 
-  componentDidMount() {
-  }
   roundedRect(x, y, width, height, radius) {
     return "M" + (x + radius/4) + " " + y + " "
       + "L" + (width - radius/4) + " " + y + " "
@@ -17,12 +15,23 @@ class UrlFormBack extends Component {
       + "L" + x + " " + (y + radius/4) + " "
       + "Q" + x + " " + y + " " + (x + radius/4) + " " + y
   }
+  topRoundedRect(x, y, width, height, radius) {
+    return "M" + (x + radius/4) + " " + y + " "
+      + "L" + (width - radius/4) + " " + y + " "
+      + "Q" + (x + width) + " " + y + " " + (x + width) + " " + (y + radius/4) + " "
+      + "L" + (x + width) + " " + (y + height) + " "
+      // + "Q" + (x + width) + " " + (y + height) + " " + (x + width - radius/4) + " " + (y + height) + " "
+      + "L" + x + " " + (y + height) + " "
+      // + "Q" + x + " " + (y + height) + " " + x + " " + (y + height - radius/4) + " "
+      + "L" + x + " " + (y + radius/4) + " "
+      + "Q" + x + " " + y + " " + (x + radius/4) + " " + y
+  }
   render() {
     const { w, h, children, idx, yellowSize, clr } = this.props;
     return (
       <g id="Canvas" fill="none">
         <g id="content">
-        <path d={this.roundedRect(0, 0, w, h, 21)} fill="white"/>
+        <path d={this.topRoundedRect(0, 0, w, h, 21)} fill="white"/>
         </g>
       </g>
     );
